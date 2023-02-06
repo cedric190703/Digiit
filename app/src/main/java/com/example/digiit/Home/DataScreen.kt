d594ce6329135621c8ab16d41e315ddd7f30e41d
Brzyski <cbrzyski2@gmail.com>
2023-02-06 12:05:45 -0500

    add buttons for tickets, wallet and data pages

--- app/src/main/java/com/example/digiit/Home/DataScreen.kt
+++ app/src/main/java/com/example/digiit/Home/DataScreen.kt
@@ -3,7 +3,10 @@ package com.example.digiit.Home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,6 +42,18 @@ fun DataScreen() {
                 contentColor = Color.White,
                 elevation = 12.dp
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                Modifier.size(70.dp),
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = Color.White,
+            ) {
+                Icon(Icons.Filled.Add,
+                    "add button",
+                    modifier = Modifier.fillMaxSize(0.5F))
+            }
         }
     )
 }
--- app/src/main/java/com/example/digiit/Home/HomeTickets.kt
+++ app/src/main/java/com/example/digiit/Home/HomeTickets.kt
@@ -1,7 +1,10 @@
 package com.example.digiit.Home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,6 +40,18 @@ fun HomeScreen() {
                 contentColor = Color.White,
                 elevation = 12.dp
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                Modifier.size(70.dp),
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = Color.White,
+            ) {
+                Icon(Icons.Filled.Add,
+                    "add button",
+                    modifier = Modifier.fillMaxSize(0.5F))
+            }
         }
     )
 }
