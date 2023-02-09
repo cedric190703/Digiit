60e034f21211d536c7b3dbded69ab867e4cd39c8
Brzyski <cbrzyski2@gmail.com>
2023-02-09 17:50:35 -0500

    modif menu screen

--- app/src/main/java/com/example/digiit/Home/MenuScreen.kt
+++ app/src/main/java/com/example/digiit/Home/MenuScreen.kt
@@ -1,22 +1,29 @@
 package com.example.digiit.Home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.gestures.scrollable
+import android.content.Context
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.digiit.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun MenuScreen() {
@@ -49,125 +56,212 @@ fun MenuScreen() {
     )
 }
 
+private val optionsList: ArrayList<OptionsData> = ArrayList()
+
 @Composable
 fun MenuContent(paddingValues: PaddingValues) {
-    Column(verticalArrangement = Arrangement.Center,
-           horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Card(
-            shape = RoundedCornerShape(8.dp),
-            backgroundColor = MaterialTheme.colors.surface,
-            border = BorderStroke(1.dp,Color.Gray),
-            modifier = Modifier
-                .padding(horizontal = 36.dp, vertical = 16.dp)
-                .align(Alignment.CenterHorizontally).fillMaxWidth()
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxHeight()
-            ) {
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Button(onClick = {},
-                        Modifier.width(450.dp),
-                        shape = RoundedCornerShape(180.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF009DDB))) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.profile),
-                            contentDescription = "Logo profile",
-                            modifier = Modifier.padding(5.dp),
-                            tint = Color.White
-                        )
-                        Text(text = "Mon compte", color = Color.White, fontSize = 16.sp)
-                    }
-                }
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Button(onClick = {},
-                        Modifier.width(450.dp),
-                        shape = RoundedCornerShape(180.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF0083E7))
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.safety),
-                            contentDescription = "Logo profile",
-                            modifier = Modifier.padding(5.dp),
-                            tint = Color.White
-                        )
-                        Text(text = "Confidentialité", color = Color.White, fontSize = 16.sp)
-                    }
-                }
-            }
+    val context = LocalContext.current.applicationContext
+    var listPrepared by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.Default) {
+            optionsList.clear()
+
+            // Add the data to optionsList
+            prepareOptionsData()
+
+            listPrepared = true
         }
-        Card(
-            shape = RoundedCornerShape(8.dp),
-            backgroundColor = MaterialTheme.colors.surface,
-            border = BorderStroke(1.dp,Color.Gray),
-            modifier = Modifier.padding(horizontal = 36.dp, vertical = 16.dp)
-                .align(Alignment.CenterHorizontally).fillMaxWidth()
+    }
+
+    if (listPrepared) {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxHeight()
-            ) {
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Button(onClick = {},
-                        Modifier.width(450.dp),
-                        shape = RoundedCornerShape(180.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF056CE9))) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.settings),
-                            contentDescription = "Logo reglages",
-                            modifier = Modifier.padding(5.dp),
-                            tint = Color.White
-                        )
-                        Text(text = "Réglages", color = Color.White, fontSize = 16.sp)
-                    }
-                }
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Button(onClick = {},
-                        Modifier.width(450.dp),
-                        shape = RoundedCornerShape(180.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF0638EB))) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.folders),
-                            contentDescription = "Logo confidentialite",
-                            modifier = Modifier.padding(5.dp),
-                            tint = Color.White
-                        )
-                        Text(text = "Mes dossiers", color = Color.White, fontSize = 16.sp)
-                    }
-                }
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    Button(onClick = {},
-                        Modifier.width(450.dp),
-                        shape = RoundedCornerShape(180.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF0118EB))) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.style),
-                            contentDescription = "Logo personnalisation",
-                            modifier = Modifier.padding(5.dp),
-                            tint = Color.White
-                        )
-                        Text(text = "Personnalisation", color = Color.White, fontSize = 16.sp)
-                    }
-                }
+
+            item {
+                // User's image, name, email and edit button
+                UserDetails(context = context)
+            }
+
+            // Show the options
+            items(optionsList) { item ->
+                OptionsItemStyle(item = item, context = context)
             }
         }
     }
+}
+
+@Composable
+private fun UserDetails(context: Context) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(72.dp),
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "The image"
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(weight = 3f, fill = false)
+                    .padding(start = 16.dp)
+            ) {
+
+                // User's name
+                Text(
+                    text = "User",
+                    style = TextStyle(
+                        fontSize = 22.sp
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = "a@a.com",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.Gray,
+                        letterSpacing = (0.8).sp
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            
+            IconButton(
+                modifier = Modifier
+                    .weight(weight = 1f, fill = false),
+                onClick = { }) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = Icons.Outlined.Edit,
+                    contentDescription = "Edit Details",
+                    tint = MaterialTheme.colors.primary
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun OptionsItemStyle(item: OptionsData, context: Context) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(enabled = true) {
+            }
+            .padding(all = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+            modifier = Modifier
+                .size(32.dp),
+            painter = painterResource(id = item.icon),
+            contentDescription = item.title,
+            tint = MaterialTheme.colors.primary
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(weight = 3f, fill = false)
+                    .padding(start = 16.dp)
+            ) {
+
+                Text(
+                    text = item.title,
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(
+                    text = item.subTitle,
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        letterSpacing = (0.8).sp,
+                        color = Color.Gray
+                    )
+                )
+
+            }
+
+            Icon(
+                modifier = Modifier
+                    .weight(weight = 1f, fill = false),
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = item.title,
+                tint = Color.Black.copy(alpha = 0.70f)
+            )
+        }
+
+    }
+}
+
+data class OptionsData(val icon: Int, val title: String, val subTitle: String)
+
+private fun prepareOptionsData() {
+    optionsList.add(
+        OptionsData(
+            icon = R.drawable.account,
+            title = "Compte",
+            subTitle = "Gérez votre compte"
+        )
+    )
+
+    optionsList.add(
+        OptionsData(
+            icon = R.drawable.safety,
+            title = "Confidentialité",
+            subTitle = "Gérez votre confidentialité"
+
+        )
+    )
+
+    optionsList.add(
+        OptionsData(
+            icon = R.drawable.settings,
+            title = "Réglages",
+            subTitle = "Les réglages"
+        )
+    )
+
+    optionsList.add(
+        OptionsData(
+            icon = R.drawable.folders,
+            title = "Vos dossiers",
+            subTitle = "Regardez vos dossiers"
+        )
+    )
+
+    optionsList.add(
+        OptionsData(
+            icon = R.drawable.style,
+            title = "Personnalisation",
+            subTitle = "Personnalisez votre application"
+        )
+    )
 }
